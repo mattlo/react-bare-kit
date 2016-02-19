0010752c16097833dea25eea8c052405055ed987
Matt Lo <mattlo.developer@gmail.com>
2016-02-18 19:35:00 -0600

    connected scss

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,8 @@
+import './Main.scss';
 import React from 'react';
 
 export default () => (
-  <div>
+  <div className="main">
     React Bare Kit by <a href="https://twitter.com/matt_lo" target="_blank">Matt Lo</a>.
   </div>
 );
